df575fcac66b985dd103ac6a13c7011111f845be
Amandeep8848 <eight848-digital@DESKTOP-ARCF17F>
2024-06-11 14:13:11 +0530

    fix: attendance validations

--- attendance_heatmap/attendance_heatmap/doctype/employee_attendance/employee_attendance.py
+++ attendance_heatmap/attendance_heatmap/doctype/employee_attendance/employee_attendance.py
@@ -1,8 +1,17 @@
 # Copyright (c) 2024, Amandeep and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import today
 
 class EmployeeAttendance(Document):
-	pass
+	def validate(self):
+		# check if the selected date is ahead of the current date
+		if self.date > today():
+			frappe.throw(_("Cannot Mark Attendance for Future Days."))
+
+		# check if the attendance is already marked
+		if frappe.db.exists("Employee Attendance",{"employee":self.employee,"date":self.date}):
+			frappe.throw(_(f"You have already marked the attendance for {self.date}"))
